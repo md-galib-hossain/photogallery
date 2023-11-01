21b320b4976dedb1384118a9f0751e3ae107aa50
md-galib-hossain <mdgalib23@gmail.com>
2023-11-02 00:49:54 +0600

    Swap functionality added

--- src/Components/Gallery/GalleryContainer.jsx
+++ src/Components/Gallery/GalleryContainer.jsx
@@ -1,16 +1,53 @@
-import image1 from "./../../../../images/image-1.webp"
-import image2 from "./../../../../images/image-2.webp"
-import image3 from "./../../../../images/image-3.webp"
-import image4 from "./../../../../images/image-4.webp"
-import image5 from "./../../../../images/image-5.webp"
-import image6 from "./../../../../images/image-6.webp"
-import image7 from "./../../../../images/image-7.webp"
-import image8 from "./../../../../images/image-8.webp"
-import image9 from "./../../../../images/image-9.webp"
-import image10 from "./../../../../images/image-10.jpeg"
-import image11 from "./../../../../images/image-11.jpeg"
+import image1 from "./../../../../images/image-1.webp";
+import image2 from "./../../../../images/image-2.webp";
+import image3 from "./../../../../images/image-3.webp";
+import image4 from "./../../../../images/image-4.webp";
+import image5 from "./../../../../images/image-5.webp";
+import image6 from "./../../../../images/image-6.webp";
+import image7 from "./../../../../images/image-7.webp";
+import image8 from "./../../../../images/image-8.webp";
+import image9 from "./../../../../images/image-9.webp";
+import image10 from "./../../../../images/image-10.jpeg";
+import image11 from "./../../../../images/image-11.jpeg";
+import { useState } from "react";
 const GalleryContainer = () => {
-  const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9,image10, image11]
+  const [draggedImage, setDraggedImage] = useState(null);
+  const [galleryImages, setGalleryImages] = useState([
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+  ]);
+  // function for getting the image index i am dragging
+  const handleDragStart = (index) => {
+    setDraggedImage(index);
+  };
+  // function for where i am putting the image
+  const handleDragOver = (index, e) => {
+    e.preventDefault();
+  };
+  // this is where i am dropping and swapping , I took help from chatgpt for this function
+  const handleDrop = (index) => {
+    if (draggedImage !== null && draggedImage !== index) {
+      setGalleryImages((prevImages) => {
+        const newGalleryImages = [...prevImages];
+        [newGalleryImages[draggedImage], newGalleryImages[index]] = [
+          newGalleryImages[index],
+          newGalleryImages[draggedImage],
+        ];
+        return newGalleryImages;
+      });
+    }
+
+    setDraggedImage(null);
+  };
   return (
     <>
       {/* Heading Section start */}
@@ -28,18 +65,21 @@ const GalleryContainer = () => {
       <div className="bg-white md:min-h-[500px] p-8">
         {/* Grid section start */}
         <div className="grid md:grid-cols-5 gap-4">
-          <div className="min-h-[60px] min-w-[60px] col-span-2 row-span-2 bg-red-500 rounded border-2 ">
-            <img src={image1} alt="" />
-          </div>
-        {
-          images?.map(image=>
-            <div className="min-h-[60px] min-w-[60px] rounded border-2 ">
-            <img src={image} alt="" />
-          </div>
-
-          )
-        }
-       
+          {galleryImages?.map((image, index) => (
+            <div
+              key={index}
+              className={`min-h-[60px] min-w-[60px] ${
+                index == 0 && "col-span-2 row-span-2"
+              } rounded border-2`}
+              onDragOver={(e) => handleDragOver(index, e)}
+              onDrop={() => handleDrop(index)}
+              draggable
+              onDragStart={() => handleDragStart(index)}
+            >
+              <span>{index}</span>
+              <img src={image} alt="" />
+            </div>
+          ))}
         </div>
         {/* Grid section end */}
       </div>
